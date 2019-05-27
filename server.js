4d153818dc2754f56c04cfc2dccd3055efaf0e02
EricPrytulla <eric.prytulla@dibuco.de>
2019-05-27 18:22:34 +0200

    moved misplaced console logsa

--- server.js
+++ server.js
@@ -7,50 +7,50 @@ var Cloudant = require('@cloudant/cloudant');
 const username = "ba52417a-4252-4ddb-ae0a-b036f6dbd38d-bluemix";
 const password = "691f57c40a74294348a78c9ac06ae40d376df804ee473c0650ffb10fa0edb5a4";
 const url = "https://ba52417a-4252-4ddb-ae0a-b036f6dbd38d-bluemix:691f57c40a74294348a78c9ac06ae40d376df804ee473c0650ffb10fa0edb5a4@ba52417a-4252-4ddb-ae0a-b036f6dbd38d-bluemix.cloudantnosqldb.appdomain.cloud";
-const cloudant = Cloudant({account:username, password:password, url: url});
+const cloudant = Cloudant({account: username, password: password, url: url});
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: false}))
 
 // parse application/json
 app.use(bodyParser.json());
 
-app.use (function (req, res, next) {
-    if (!req.secure|| process.env.BLUEMIX_REGION === undefined) {
+app.use(function (req, res, next) {
+    if (!req.secure || process.env.BLUEMIX_REGION === undefined) {
         next();
     } else {
         res.redirect('https://' + req.headers.host + req.url);
     }
 });
 
-app.put('/user', function(req, res){
-      var users = cloudant.db.use('users');
-      console.log('JSON-user ' + JSON.stringify(req.body));
-      users.insert(req.body, req.body.user, function(err, body, header) {
+app.put('/user', function (req, res) {
+    var users = cloudant.db.use('users');
+    console.log('JSON-user ' + JSON.stringify(req.body));
+    users.insert(req.body, req.body.user, function (err, body, header) {
         if (err) {
-          res.sendStatus(400);
-          console.error('[user.insert] ', err.message);
-        }else {
-          res.send(body);
+            res.sendStatus(400);
+            console.error('[user.insert] ', err.message);
+        } else {
+            console.log('You have inserted user ' + req.body.user);
+            console.log(body);
+            res.send(body);
         }
-        console.log('You have inserted user ' + req.body.user);
-        console.log(body);
-      });
     });
+});
 
-app.get('/user/:id', function(req, res){
-  var users = cloudant.db.use('users');
-  users.get(req.params.id, function(err, body, header) {
-    if (err) {
-      res.sendStatus(400);
-      console.error('[user.get] ', err.message);
-    }else {
-      res.send(body);
-    }
-    console.log('You have read user ' + body.id);
-  });
+app.get('/user/:id', function (req, res) {
+    var users = cloudant.db.use('users');
+    users.get(req.params.id, function (err, body, header) {
+        if (err) {
+            res.sendStatus(400);
+            console.error('[user.get] ', err.message);
+        } else {
+            console.log('You have read user ' + body.id);
+            res.send(body);
+        }
+    });
 });
 let port = process.env.PORT || 3100;
-app.listen(port, function() {
+app.listen(port, function () {
     console.log("Listening to port: " + port);
 });
