7951ef2f43e64300f4f081419325c3af98871895
EricPrytulla <eric.prytulla@dibuco.de>
2019-06-04 00:43:06 +0200

    idk

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.delete('/disconnect', function (req, res) {
     var users = cloudant.db.use('connected_users');
     console.log('JSON-user ' + JSON.stringify(req.body));
     users.head(req.body.id).then(headers => {
-        console.log(JSON.stringify(rev));
+        console.log(JSON.stringify(headers));
         users.destroy(req.body.id, headers.etag, function (err, body, header) {
             if (err) {
                 res.sendStatus(400);
